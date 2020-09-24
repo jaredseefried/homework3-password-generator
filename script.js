d3356873c20af1398f65276790fa3107c190ab19
jaredcito <jared.seefried@yahoo.com>
2020-09-24 17:44:19 -0600

    Added variables

---
+++ script.js
@@ -0,0 +1,20 @@
+
+
+var passwordValues = {
+    Lowletters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+    capLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    num = ["0", "1", "2","3", "4", "5", "6", "7", "8", "9"],
+    specChar = ["!", "@", "#"]
+}
+
+var confirmLength = "";
+var confirmSpecialCharacter;
+var confirmNumericCharacter;
+var confirmUpperCase;
+var confirmLowerCase;
+
+ function createPassword (){
+     var confirmLength = ("Please choose from between 8 and 128 characters");
+ }
+
+ confirmLength();
